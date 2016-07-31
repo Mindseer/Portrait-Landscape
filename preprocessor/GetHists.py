0093315e97f2ffe7ba81d05d6992fa64c9555588
Mindseer <max@maxgrant.com.au>
2016-07-31 14:03:54 +1000

    Clean data, fancy image clicking
    Can now click images to get a popup. Cleaned data to remove incomplete
    embedded markup.

--- preprocessor/GetHists.py
+++ preprocessor/GetHists.py
@@ -9,8 +9,8 @@ files_json = []
 
 image_count = 0
 
-parser = lxml.etree.XMLParser(recover=True)
-tree = lxml.etree.parse('portraitau-20160705.xml', parser);
+parser = lxml.etree.XMLParser(recover=True, collect_ids=False, remove_blank_text=True, resolve_entities=False)
+tree = lxml.etree.parse('portraitau-20160705.xml', parser)
 
 for portrait in tree.iter('portrait'):
     for image in portrait.iter('image'):
@@ -21,10 +21,16 @@ for portrait in tree.iter('portrait'):
             current_filename = current_filename[0:-4]
             brightness = image_original.sum() / image_original.size
             file_json = {'id': current_filename, 'brightness': brightness}
-            for date_created in portrait.iter('datecreated'):
-                file_json["date_created"] = date_created.text
-            for date_created in portrait.iter('media'):
-                file_json["media"] = date_created.text
+            for feature in portrait.iter('title'):
+                file_json["title"] = feature.text
+            for feature in portrait.iter('label'):
+                file_json["label"] = feature.text
+            for feature in image.iter('fileURL'):
+                file_json["url"] = feature.text
+            for feature in portrait.iter('datecreated'):
+                file_json["date_created"] = feature.text
+            for feature in portrait.iter('media'):
+                file_json["media"] = feature.text
             files_json = files_json + [file_json]
             if image_count % 100 == 0:
                 print(image_count)
